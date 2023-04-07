67b2fdadd4138208c6fe2dca163d52f6de3b9667
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-07 20:39:40 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
-import Header from './components/Header'
+import Header from './components/layout/Header'
 import Nav from './components/Nav'
 import Main from './components/Main'
-import Footer from './components/Footer'
+import Footer from './components/layout/Footer'
 
 function App() {
 	return (
 		<>
 			<Header/>
-			<Nav/>
-			<Main/>
 			<Footer/>
 		</>
 	);
