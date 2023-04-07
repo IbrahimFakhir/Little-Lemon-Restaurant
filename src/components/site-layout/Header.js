225f32e35861f32fe7faf710cadbb4a306e6e5e4
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-07 23:52:38 +0200

    optimized imports

--- src/components/site-layout/Header.js
+++ src/components/site-layout/Header.js
@@ -1,9 +1,15 @@
-import Logo from "./assets/logo.png"
+import {useState} from "react"
+import {useLocation} from "react-router-dom"
+import LogoImage from "./assets/logo.png"
+import "./Header.css"
 
 export default function Header() {
+    const {pathname} = useLocation();
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+    
     return (
         <header>
-            <img src={Logo} alt="Little Lemon Logo" />
+            <img src={LogoImage} alt="Little Lemon Logo" />
         </header>
     )
 }
