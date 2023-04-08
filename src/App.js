75162dba0e51dd9ce3dc840cd2298980519e5f7b
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 13:38:02 +0200

    included BrowserRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Header from './components/site-layout/Header'
 import Footer from './components/site-layout/Footer'
-import Layout from './components/site-layout/layout';
+import Layout from './components/site-layout/layout'
+
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 	return (
