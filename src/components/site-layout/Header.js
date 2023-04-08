ebc5c0e0c949af8aed37514e6334f9124944492d
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 13:59:55 +0200

    optimized imports in Header.js

--- src/components/site-layout/Header.js
+++ src/components/site-layout/Header.js
@@ -1,10 +1,12 @@
 import {useState} from "react"
 import {useLocation} from "react-router-dom"
-import LogoImage from "./assets/logo.png"
+import pages from '../../utils/pages'
 import "./Header.css"
-
+import LogoImage from "./assets/logo.png"
 import {faBars, faXmark} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const navLinks = Array.from(pages.values()).filter(page => page.anchorable)
 
 export default function Header() {
     const {pathname} = useLocation();
