729495ae2f15bceb788479a82edeab1b0f1707d4
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 18:26:18 +0200

    implemented NotFound page

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -1,7 +1,9 @@
+import "./index.css"
+
 export default function NotFound() {
     return (
-        <>
-            
-        </>
+        <div className="container page-not-found">
+            <h2>Page not found</h2>
+        </div>
     )
 }
