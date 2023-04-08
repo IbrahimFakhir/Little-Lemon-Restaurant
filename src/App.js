04c0dd032411e9bc7338a16b0c383228e7735c4f
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 16:04:19 +0200

    optimized imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import pages from './utils/pages'
-
 import Layout from './components/site-layout/Layout'
+
 import Home from './components/pages/Home'
 import Reservations from './components/pages/Reservations'
+import ConfirmedReservation from './components/pages/Reservations/ConfirmedReservation'
+import NotFound from './components/pages/NotFound'
 import UnderConstruction from './components/pages/UnderConstruction'
 
 function App() {
