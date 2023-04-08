6b4b71d559f4c047bc15405a694338d8097c7061
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 14:09:39 +0200

    fixed typos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/site-layout/Header'
 import Footer from './components/site-layout/Footer'
-import Layout from './components/site-layout/layout'
+import Layout from './components/site-layout/Layout'
 
 import {Routes, Route} from "react-router-dom"
 
