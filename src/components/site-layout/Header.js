4bc443e4170d13b39df432c17462d1471e4eb4f7
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 13:22:44 +0200

    optimized imports

--- src/components/site-layout/Header.js
+++ src/components/site-layout/Header.js
@@ -3,6 +3,9 @@ import {useLocation} from "react-router-dom"
 import LogoImage from "./assets/logo.png"
 import "./Header.css"
 
+import {faBars, faXmark} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 export default function Header() {
     const {pathname} = useLocation();
     const [isNavExpanded, setIsNavExpanded] = useState(false);
