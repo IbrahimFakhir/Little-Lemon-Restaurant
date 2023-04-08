babf2ef40c0aa9559f8e48591509edc5631bd4f7
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 21:17:55 +0200

    implemented rest of Home page

---
+++ src/components/pages/Home/TestimonialCard.js
@@ -0,0 +1,20 @@
+import { faStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './TestimonialCard.css';
+
+const ratingLevels = { '0.5': faStarHalfStroke, '1': faStar, };
+
+export default function TestimonialCard ({customer}) {
+    return (
+        <article className="testimonial-card">
+            <img src={ customer.image } alt={customer.fullName} />
+            <h4>{ customer.fullName }</h4>
+            <span>
+                {customer.rating.map((ratingPoint, index) =>
+                    <FontAwesomeIcon key={index} icon={ratingLevels[ratingPoint]} size="xs" />
+                )}
+            </span>
+            <blockquote><p>"{ customer.says }"</p></blockquote>
+        </article>
+    )
+}
