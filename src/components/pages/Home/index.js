31dcf3ffbe69efc6c6ab1238f15948873993f8db
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 18:59:29 +0200

    implemented index.js for Home page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,7 +1,15 @@
+import Hero from "./Hero"
+import Highlights from "./Highlights"
+import Testimonials from "./Testimonials"
+import About from "./About"
+
 export default function Home() {
     return (
         <>
-            
+            <Hero />
+            <Highlights />
+            <Testimonials />
+            <About />
         </>
     )
 }
