77249b9fe801bda86e329387fdf752c8beff7891
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 17:03:34 +0200

    finished Header routing

--- src/components/site-layout/Header.js
+++ src/components/site-layout/Header.js
@@ -5,6 +5,7 @@ import "./Header.css"
 import LogoImage from "./assets/logo.png"
 import {faBars, faXmark} from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link } from "react-router-dom"
 
 const navLinks = Array.from(pages.values()).filter(page => page.anchorable)
 
@@ -18,12 +19,26 @@ export default function Header() {
                 <Link className="nav-bar-logo" to={ pages.get('home').path }>
                     <img src={ LogoImage } alt="Little Lemon Logo" />
                 </Link>
-                <button className="nav-bar-hamburger" type="button" onClick={ setIsNavExpanded(!isNavExpanded) }>
+                <button className="nav-bar-hamburger" type="button" onClick={ () => setIsNavExpanded(!isNavExpanded) }>
                     { isNavExpanded ? 
                     <FontAwesomeIcon icon={ faXmark } /> :
                     <FontAwesomeIcon icon={ faBars } /> }
                 </button>
-
+                <ul
+                    className={ isNavExpanded ? 'nav-bar-links expanded' : 'nav-bar-links' }
+                    onClick={ () => setIsNavExpanded(!isNavExpanded) }
+                >
+                    {navLinks.map((navLink, index) => 
+                        <li key={index}>
+                            <Link
+                                className={ pathname === navLink.path ? 'current-location' : '' }
+                                to={ navLink.path }
+                            >
+                                {navLink.name}
+                            </Link>
+                        </li>
+                    )}
+                </ul>
             </nav>
         </header>
     )
