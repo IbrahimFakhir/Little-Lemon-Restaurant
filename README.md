0f750920357775421e43e2e46520d7adcf0274c3
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 18:18:48 +0200

    implemented footer

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - npm install react-router-dom@6
 - npm install @fortawesome/react-fontawesome
 - npm install @fortawesome/free-solid-svg-icons
+- npm install @fortawesome/free-brands-svg-icons
--- src/components/site-layout/Footer.js
+++ src/components/site-layout/Footer.js
@@ -1,33 +1,67 @@
+import { faFacebook, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import './Footer.css'
 import logoWhite from "./assets/logo-white.png"
+import pages from "../../utils/pages"
+
+const contacts = [
+    { icon: faLocationDot, info: '678 Pisa Ave, Chicago, IL 60611', },
+    { icon: faPhone, info: '(312) 593-2744', },
+    { icon: faEnvelope, info: 'customer@littlelemon.com', },
+];
+
+const socials = [
+    { icon: faFacebook, name: 'facebook', },
+    { icon: faTwitter, name: 'twitter', },
+    { icon: faInstagram, name: 'instagram', },
+    { icon: faYoutube, name: 'youtube', },
+];
+
+const navLinks = Array.from(pages.values()).filter(page => page.anchorable);
 
 export default function Footer() {
     return (
-        <footer>
-            <div className="container-grid">
+        <footer className="site-footer">
+            <div className="container grid">
                 <img 
-                    className="site-footer-logo" 
-                    src={logoWhite} 
-                    alt="Little Lemon Logo" 
+                    className="site-footer-logo"
+                    src={ logoWhite }
+                    alt="Little Lemon"
                 />
                 <nav className="site-footer-nav">
                     <h4>Sitemap</h4>
                     <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Menu</li>
-                        <li>Reservations</li>
-                        <li>Order Online</li>
-                        <li>Login</li>
+                        {navLinks.map((navLink, index) => 
+                            <li key={ index }>
+                                <Link to={ navLink.path }>{ navLink.name }</Link>
+                            </li>
+                        )}
                     </ul>
                 </nav>
-                <div className="site-footer-contact">
+                <div className='site-footer-contact'>
                     <h4>Contact us</h4>
-                    <p>678 Pisa Ace, Chicago, IL 60611</p>
-                    <p>(312) 593-2744</p>
-                    <p>customer@littlelemon.com</p>
+                    <address>
+                        {contacts.map((contact, index) => 
+                            <p key={index}>
+                                <FontAwesomeIcon icon={ contact.icon } /> {contact.info}
+                            </p>
+                        )}
+                    </address>
                 </div>
-                <div className="site-footer-social">
+                <div className='site-footer-social'>
                     <h4>Connect with us</h4>
+                    {socials.map((social, index) => 
+                        <a
+                            key={ index }
+                            href={ `http://www.${social.name}.com` }
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FontAwesomeIcon icon={ social.icon } size="lg" />
+                        </a>
+                    )}
                 </div>
             </div>
         </footer>
