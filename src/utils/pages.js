8743ec13a316d79c62d7954f5b5706830c8da11b
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-08 13:50:27 +0200

    added pages.js

---
+++ src/utils/pages.js
@@ -0,0 +1,16 @@
+const pages = new Map()
+
+pages.set('home', { name: 'Home', path: '/', anchorable: true })
+
+pages.set('about', { name: 'About', path: '/about', anchorable: true })
+
+pages.set('menu', { name: 'Menu', path: '/menu', anchorable: true })
+
+pages.set('reservations', { name: 'Reservations', path: '/reservations', anchorable: true })
+pages.set('confirmedReservation', { name: 'Confirmed Reservation', path: '/confirmed-reservation', anchorable: false })
+
+pages.set('orderOnline', { name: 'Order Online', path: '/order-online', anchorable: true })
+
+pages.set('login', { name: 'Login', path: '/login', anchorable: true })
+
+export default pages;
