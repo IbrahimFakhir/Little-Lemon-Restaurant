3e42c7da10fa91c978b5dbd855f418230dc3bb08
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-25 00:47:58 +0200

    setup of available date and time

--- src/components/pages/Reservations/index.js
+++ src/components/pages/Reservations/index.js
@@ -5,6 +5,13 @@ import { fetchAPI, submitAPI } from "../../../utils/fakeAPI";
 import pages from '../../../utils/pages';
 import BookingForm from './ReservationForm'
 
+const updateTimes = (availableTimes, date) => {
+    const response = fetchAPI(new Date(date));
+    return (response.length !== 0) ? response : availableTimes;
+}
+
+const initializeTimes = initialAvailableTimes => [...initialAvailableTimes, ...fetchAPI(new Date())];
+
 export default function Reservation() {
     return (
         <>
