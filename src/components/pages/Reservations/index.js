a6155bc915045f4204273be1bb2c06f1e66f8d75
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-25 00:41:47 +0200

    added infrastructure for Reservations

--- src/components/pages/Reservations/index.js
+++ src/components/pages/Reservations/index.js
@@ -1,3 +1,10 @@
+import { useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+import './index.css';
+import { fetchAPI, submitAPI } from "../../../utils/fakeAPI";
+import pages from '../../../utils/pages';
+import BookingForm from './BookingForm'
+
 export default function Reservation() {
     return (
         <>
