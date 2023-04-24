ab5d9913bd9b797be8be2bc2a96e2d269049c0ac
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-04-25 00:42:58 +0200

    fixed typo

--- src/components/pages/Reservations/index.js
+++ src/components/pages/Reservations/index.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import './index.css';
 import { fetchAPI, submitAPI } from "../../../utils/fakeAPI";
 import pages from '../../../utils/pages';
-import BookingForm from './BookingForm'
+import BookingForm from './ReservationForm'
 
 export default function Reservation() {
     return (
