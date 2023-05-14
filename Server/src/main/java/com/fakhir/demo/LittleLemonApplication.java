9f86fb61727f41ab16dc5fc124d3ad7f6a9c4307
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-14 16:55:02 +0200

    refactoring

---
+++ Server/src/main/java/com/fakhir/demo/LittleLemonApplication.java
@@ -0,0 +1,13 @@
+package com.fakhir.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleLemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleLemonApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/fakhir/demo/LittleLemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakhir.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleLemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
