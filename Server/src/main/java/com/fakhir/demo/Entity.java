6bbbb3ac9f3ece6399f49d8903e3014d570589f9
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-14 17:46:44 +0200

    added Entitiy and h2 db

---
+++ Server/src/main/java/com/fakhir/demo/Entity.java
@@ -0,0 +1,18 @@
+package com.fakhir.demo;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@jakarta.persistence.Entity
+public class Entity {
+    @Id
+    @GeneratedValue
+    Integer id;
+    @Column
+    Date date;
+    boolean available;
+}
