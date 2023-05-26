eec15575e3133d447107384f0a2ff6076bc85fe6
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-05-26 20:23:10 +0200

    visual enhancements for previous order

--- Client/src/components/pages/Reservations/ReservationDetails.js
+++ Client/src/components/pages/Reservations/ReservationDetails.js
@@ -1,35 +1,45 @@
 export default function ReservationDetails({ reservation, index }) {
     return (
         <div className="order">
-            <h3>Your {index+1}. Order</h3>
+            <h3>Your {index + 1}. Order</h3>
             <div className="order-details">
-                <div className="order-details-column">
-                <p>
-                    <strong>First Name:</strong> {reservation.name.split(" ")[0]}
-                </p>
-                <p>
-                    <strong>Last Name:</strong> {reservation.name.split(" ")[1]}
-                </p>
-                <p>
-                    <strong>E-mail:</strong> {reservation.email}
-                </p>
-                <p>
-                    <strong>Telephone:</strong> {reservation.telephone}
-                </p>
+                <div className="personal-details">
+                <h4>Personal Details</h4>
+                <div className="order-details-item">
+                    <label>First Name:</label>
+                    <span>{reservation.name.split(" ")[0]}</span>
                 </div>
-                <div className="order-details-column">
-                <p>
-                    <strong>Date:</strong> {reservation.date.split(" ")[0]}
-                </p>
-                <p>
-                    <strong>Time:</strong> {reservation.date.split(" ")[1]}
-                </p>
-                <p>
-                    <strong>Occasion:</strong> {reservation.occasion}
-                </p>
-                <p>
-                    <strong>Number of Guests:</strong> {reservation.numGuests}
-                </p>
+                <div className="order-details-item">
+                    <label>Last Name:</label>
+                    <span>{reservation.name.split(" ")[1]}</span>
+                </div>
+                <div className="order-details-item">
+                    <label>E-mail:</label>
+                    <span>{reservation.email}</span>
+                </div>
+                <div className="order-details-item">
+                    <label>Telephone:</label>
+                    <span>{reservation.telephone}</span>
+                </div>
+                </div>
+                <div className="reservation-details">
+                <h4>Reservation Details</h4>
+                <div className="order-details-item">
+                    <label>Date:</label>
+                    <span>{reservation.date.split(" ")[0]}</span>
+                </div>
+                <div className="order-details-item">
+                    <label>Time:</label>
+                    <span>{reservation.date.split(" ")[1]}</span>
+                </div>
+                <div className="order-details-item">
+                    <label>Occasion:</label>
+                    <span>{reservation.occasion}</span>
+                </div>
+                <div className="order-details-item">
+                    <label>Number of Guests:</label>
+                    <span>{reservation.numGuests}</span>
+                </div>
                 </div>
             </div>
         </div>
